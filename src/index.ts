97f1f5caf7eb1ebfb452a23285c9aff3602b37a8
Kenichi Takahashi <kenichi.taka@gmail.com>
2024-07-23 19:46:58 +0900

    parse rss using fast-xml-parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
+import { XMLParser } from 'fast-xml-parser'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  const rssUrl = 'https://kenchan.github.io/diary.rss'
+  const response = await fetch(rssUrl)
+  const xmlData = await response.text()
+  const parser = new XMLParser()
+  const feed = parser.parse(xmlData)
+  const firstEntry = feed.rss.channel.item[0]
+  return c.json(firstEntry)
 })
 
 export default app
